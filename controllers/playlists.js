9f482c67c9b9d172aec3e5bf896d84a41d09e0b8
Anjan Rai <anjanraiz@Anjans-MacBook-Pro.local>
2021-05-09 13:18:33 +1000

    api to delete playlist added

--- controllers/playlists.js
+++ controllers/playlists.js
@@ -1,7 +1,8 @@
 const db = require('../initialiseDB');
 const fs = require('fs');
 const { successResponse, errorResponse } = require('../middlewares/responseFormat');
-const {getNewId} = require("../utils/getNewId");
+const {getNewId, getRequiredData, sanitisePlaylistData} = require("../utils/helpers");
+const {playlistFilePath} = require("../constants/index");
 
 
 const getPlaylists = (req, res) => {
@@ -22,10 +23,10 @@ const createPlaylist = (req, res) => {
     } else {
         newPlaylist.id = parseInt(req.body.id)
     }
+    newPlaylist = sanitisePlaylistData(newPlaylist);
     try{
         db.playlists.push(newPlaylist);
-        let data = JSON.stringify(db.playlists, null, 4);
-        console.log(data);
+        let data = JSON.stringify(db.playlists, null, 2);
         fs.writeFileSync('./data/playlists.json', data);
     } catch(error) {
         return res.status(500).json(errorResponse(error.message, res.statusCode));
@@ -33,32 +34,51 @@ const createPlaylist = (req, res) => {
     res.status(201).json(successResponse("Playlist created", db.playlists, res.statusCode));
 }
 
+
 const updatePlaylist = (req, res) => {
     let playlistId = parseInt(req.params.id);
-    let requiredPlaylist = db.playlists.find( playlist => playlist.id === playlistId);
+    let requiredPlaylist = getRequiredData(db.playlists, playlistId);
     if(requiredPlaylist){
         for (const [key, value] of Object.entries(req.body)) {
             requiredPlaylist[`${key}`] = value;
         }
-        // requiredPlaylist = req.body;
         try {
             let fileContent = JSON.parse(fs.readFileSync('./data/playlists.json'));
             fileContent = fileContent.filter(content => content.id !== requiredPlaylist.id)
-            console.log(fileContent);
             fileContent.push(requiredPlaylist);
             fs.writeFileSync('./data/playlists.json', JSON.stringify(fileContent, null, 2));
-            console.log(fileContent);
         } catch (error) {
-            console.log(error);
-            return res.status(400).json(errorResponse("Failed to update playlist."));
+            return res.status(400).json(errorResponse(error.message, res.statusCode));
         }
         return res.status(200).json(successResponse("Playlist updated successfully", requiredPlaylist, res.statusCode));
     }
     res.status(400).json(errorResponse("Playlist not found to update.", res.statusCode));
 }
 
+
+const deletePlaylist = (req, res) => {
+    let playlistId = parseInt(req.params.id);
+    let requiredPlaylist = getRequiredData(db.playlists, playlistId);
+    if(requiredPlaylist){
+        let updatedPlaylists = db.playlists.filter(playlist => playlist.id !== requiredPlaylist.id);
+        updatedPlaylists = JSON.stringify(updatedPlaylists, null, 2);
+        console.log("Updated playlist", updatedPlaylists);
+        try {
+            console.log(playlistFilePath);
+            fs.writeFileSync(playlistFilePath, updatedPlaylists)
+            return res.status(204).json(successResponse("Playlist deleted successfully.", res.statusCode));
+        } catch (error) {
+            return res.status(500).json(errorResponse(error.message, res.statusCode));
+        }
+    }
+    else {
+        return res.status(400).json(errorResponse("Playlist not found to delete", res.statusCode));
+    }
+}
+
 module.exports = {
     getPlaylists,
     createPlaylist,
-    updatePlaylist
+    updatePlaylist,
+    deletePlaylist
 };
