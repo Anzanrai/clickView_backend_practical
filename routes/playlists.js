b3d5bf27cb6bb6d045b217b575cd0898ec791a27
Anjan Rai <anjanraiz@Anjans-MacBook-Pro.local>
2021-05-09 12:25:35 +1000

    routes for playlist updated

--- routes/playlists.js
+++ routes/playlists.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 
-const {getPlaylists} = require("../controllers/playlists");
+const {getPlaylists, createPlaylist} = require("../controllers/playlists");
 
 router.get('', getPlaylists);
+router.post('', createPlaylist);
 
 module.exports = router;
