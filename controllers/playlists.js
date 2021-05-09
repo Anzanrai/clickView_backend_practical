09107cf33113d84aaecb1acdbc11a0f1413bff7e
Anjan Rai <anjanraiz@Anjans-MacBook-Pro.local>
2021-05-09 12:49:42 +1000

    added controller logic to update a playlist

--- controllers/playlists.js
+++ controllers/playlists.js
@@ -26,14 +26,39 @@ const createPlaylist = (req, res) => {
         db.playlists.push(newPlaylist);
         let data = JSON.stringify(db.playlists, null, 4);
         console.log(data);
-        fs.writeFileSync('./data/playlists.json', JSON.stringify(db.playlists));
+        fs.writeFileSync('./data/playlists.json', data);
     } catch(error) {
         return res.status(500).json(errorResponse(error.message, res.statusCode));
     }
-    // initialiseDb();
     res.status(201).json(successResponse("Playlist created", db.playlists, res.statusCode));
 }
+
+const updatePlaylist = (req, res) => {
+    let playlistId = parseInt(req.params.id);
+    let requiredPlaylist = db.playlists.find( playlist => playlist.id === playlistId);
+    if(requiredPlaylist){
+        for (const [key, value] of Object.entries(req.body)) {
+            requiredPlaylist[`${key}`] = value;
+        }
+        // requiredPlaylist = req.body;
+        try {
+            let fileContent = JSON.parse(fs.readFileSync('./data/playlists.json'));
+            fileContent = fileContent.filter(content => content.id !== requiredPlaylist.id)
+            console.log(fileContent);
+            fileContent.push(requiredPlaylist);
+            fs.writeFileSync('./data/playlists.json', JSON.stringify(fileContent, null, 2));
+            console.log(fileContent);
+        } catch (error) {
+            console.log(error);
+            return res.status(400).json(errorResponse("Failed to update playlist."));
+        }
+        return res.status(200).json(successResponse("Playlist updated successfully", requiredPlaylist, res.statusCode));
+    }
+    res.status(400).json(errorResponse("Playlist not found to update.", res.statusCode));
+}
+
 module.exports = {
     getPlaylists,
-    createPlaylist
+    createPlaylist,
+    updatePlaylist
 };
