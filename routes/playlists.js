0a1b5badaf64cd914a970d3ec6301da36aa8e002
Anjan Rai <anjanraiz@Anjans-MacBook-Pro.local>
2021-05-09 12:50:02 +1000

    added route to patch playlist

--- routes/playlists.js
+++ routes/playlists.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 
-const {getPlaylists, createPlaylist} = require("../controllers/playlists");
+const {getPlaylists, createPlaylist, updatePlaylist} = require("../controllers/playlists");
 
 router.get('', getPlaylists);
 router.post('', createPlaylist);
+router.patch('/:id', updatePlaylist);
 
 module.exports = router;
