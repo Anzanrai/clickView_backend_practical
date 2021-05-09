55cf160899e7235c6fb2d099af1486df57082d8b
Anjan Rai <anjanraiz@Anjans-MacBook-Pro.local>
2021-05-09 12:24:27 +1000

    updated to use playlists routes

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const PORT = 8080;
 const db = require('./initialiseDB');
 
 app.use(express.json());
-
+app.use('/api/playlists', require('./routes/playlists'));
 
 app.listen(PORT, () => {
-    console.log(`[Playlists]: ${db.playlists}`)
+    console.log(db.playlists);
     console.log(`Server runnning at the port ${PORT}`)
 })
